667dc641efa23c4871b848e02d8289e7c1c528de
loureto <loureto.tads123@gmail.com>
2021-12-04 13:18:31 -0400

    componentização

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/button.scss';
+
+export const Button = () => {
+    return(
+        <React.Fragment>
+            <button className="button">Converter</button>
+        </React.Fragment>
+    );
+}
---
+++ src/components/Footer/Header.tsx
@@ -0,0 +1,9 @@
+import '../../styles/header.scss';
+
+export const Header = () => {
+    return(
+        <header className="header">
+            <h1>Conversor de Moeda</h1>
+        </header>    
+    );
+}
