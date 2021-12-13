8846bd0d9439d4483f348e3215e5341b7ba8f9df
loureto <loureto.tads123@gmail.com>
2021-12-13 16:47:26 -0400

    refatoracao data/hora

--- src/components/Converted/Converted.js
+++ src/components/Converted/Converted.js
@@ -4,8 +4,8 @@ import './converted.scss';
 
 export const Converted = (props) =>{
     const date = new Date();
-    let horas = `${date.getHours()}:${date.getMinutes()}`;
-    let data = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+    let horas = new Intl.DateTimeFormat('pt-BR', {timeStyle: 'medium'}).format(date);
+    let data = new Intl.DateTimeFormat('pt-BR', {dateStyle: 'medium'}).format(date);
 
     console.log(Intl.DateTimeFormat('pt-BR').format(date));
 
@@ -13,7 +13,7 @@ export const Converted = (props) =>{
         <div className="content-converted">
             <div>
                 <Label label="Data/hora" />
-                <InputText type="text" id="inputDataHora" value={`${data}   ${horas}`}/>
+                <InputText type="text" id="inputDataHora" value={`${data} - ${horas}`}/>
                 <Label label={props.name} />
                 <InputText type="text" id="inputMoeda" value={props.value}/>
                 <h2>Moeda convertida: {props.valorconvertido}</h2>
